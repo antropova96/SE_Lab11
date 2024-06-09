3abf4df98bcf42fa111c5c789d02356f4571c75b
alya-samoylenko-1994 <alya.samoylenko.1994@mail.ru>
2024-06-09 23:53:15 +0300

    updated tests

--- test_var_IX.py
+++ test_var_IX.py
@@ -1,36 +1,40 @@
 from var_IX import count_prices
 
+data = [
+    ['', '', '1', '', 'male', '', '', '', '', '100', '', ''],
+    ['', '', '1', '', 'male', '', '', '', '', '100', '', ''],
+    ['', '', '1', '', 'female', '', '', '', '', '100', '', ''],
+    ['', '', '2', '', 'male', '', '', '', '', '50', '', ''],
+    ['', '', '2', '', 'male', '', '', '', '', '50', '', ''],
+    ['', '', '2', '', 'female', '', '', '', '', '50', '', ''],
+    ['', '', '3', '', 'male', '', '', '', '', '25', '', ''],
+    ['', '', '3', '', 'male', '', '', '', '', '25', '', ''],
+    ['', '', '3', '', 'female', '', '', '', '', '25', '', ''],
+    ['', '', '3', '', 'female', '', '', '', '', '25', '', '']
+]
 
-def test_count_prices_structure():
-    data = []
-    answer = count_prices(data)
+
+def test_count_prices_default():
+    answer = count_prices(data, filter=None)
     assert answer == {
         'Класс обслуживания': ['1', '2', '3'],
-        'Цена билета (муж.)': [0, 0, 0],
-        'Цена билета (жен.)': [0, 0, 0]
+        'Цена билета': [300.0, 150.0, 100.0],
     }
 
 
-def test_count_prices_no_sex():
-    data = [list(str(i) for i in range(10)) for _ in range(10)]
-    for i in range(len(data)):
-        data[i][4] = None
+def test_count_prices_no_male():
 
-    answer = count_prices(data)
-    assert answer['Цена билета (муж.)'] == [0, 0, 0]
-    assert answer['Цена билета (жен.)'] == [0, 0, 0]
+    answer = count_prices(data, filter='муж.')
+    assert answer['Цена билета'] == [200.0, 100.0, 50.0]
 
 
-def test_count_prices_common():
-    data = [list(str(i) for i in range(10)) for _ in range(10)]
-    for i in range(len(data)):
-        data[i][4] = 'male' if i % 2 == 0 else 'female'
+def test_count_prices_no_female():
 
-    data[1][2] = '1'
-    data[2][2] = '2'
-    data[3][2] = '3'
-    data[4][2] = '1'
+    answer = count_prices(data, filter='жен.')
+    assert answer['Цена билета'] == [100.0, 50.0, 50.0]
 
-    answer = count_prices(data)
-    assert answer['Цена билета (муж.)'] == [9.0, 36.0, 0]
-    assert answer['Цена билета (жен.)'] == [9.0, 27.0, 9.0]
+
+def test_count_prices_empty():
+    data = []
+    answer = count_prices(data, filter=None)
+    assert answer['Цена билета'] == [0, 0, 0]
