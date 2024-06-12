f10c55b6f52193f9c24dd833ed39541a041e3378
antropova96 <antropova28061996@mail.ru>
2024-06-13 00:47:14 +0300

    3.1-3.2 removed read_user_input func and mergerd count_passengers and count_survivours

--- var_IV.py
+++ var_IV.py
@@ -1,24 +1,8 @@
-import csv
 import streamlit as st
 import matplotlib.pyplot as plt
 
 
-def read_user_input():
-    pclass = st.radio('Класс пассажира', ['Любой', 1, 2, 3])
-    return pclass
-
-
-def read_csv_data():
-    data = []
-    with open('data.csv') as file:
-        csv_reader = csv.reader(file)
-        next(csv_reader)  # читаем со второй строки
-        for line in csv_reader:
-            data.append(line)
-    return data
-
-
-def count_passengers(csv_data, pclass_filter='Любой') -> dict:
+def count_passengers(csv_data, p_class) -> dict:
     data = {
         'under_30': 0,
         'above_60': 0,
@@ -34,9 +18,8 @@ def count_passengers(csv_data, pclass_filter='Любой') -> dict:
         if 30 < age < 60:
             continue
 
-        pclass = int(line[2])  # применяем фильтр
-        if pclass_filter != 'Любой':
-            if pclass != pclass_filter:
+        if p_class != 'Любой':
+            if line[2] != p_class:
                 continue
 
         data['all'] += 1
@@ -47,10 +30,6 @@ def count_passengers(csv_data, pclass_filter='Любой') -> dict:
             elif age > 60.0:
                 data['above_60'] += 1
 
-    return data
-
-
-def count_survival_rate(data: dict) -> dict:
     return {
         'survival rate under 30': round(data['under_30'] / data['all'] * 100),
         'survival rate above 60': round(data['above_60'] / data['all'] * 100)
@@ -72,13 +51,9 @@ def make_figure(data):
     st.pyplot(fig)
 
 
-def main():
-    pclass_filter = read_user_input()
-    data = read_csv_data()
-    survivors = count_survival_rate(count_passengers(
-        data,
-        pclass_filter
-    ))
+def make_iv(data):
+    p_class = st.radio('Класс пассажира', ['Любой', '1', '2', '3'])
+    survivors = count_passengers(data, p_class)
     st.table(
         {
             'Возрастная группа': ['До 30 лет', 'Старше 60 лет'],
@@ -89,7 +64,3 @@ def main():
         }
     )
     make_figure(survivors)
-
-
-if __name__ == '__main__':
-    main()
