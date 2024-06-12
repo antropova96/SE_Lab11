3d05b61250f7880de80a35d604cdac7929590e0c
antropova96 <antropova28061996@mail.ru>
2024-06-13 01:07:56 +0300

    updated tests

--- test_var_IV.py
+++ test_var_IV.py
@@ -1,4 +1,4 @@
-from var_IV import count_passengers, count_survival_rate
+from var_IV import count_passengers
 
 data = [
         ['', '0', '3', '', '', '22'],
@@ -13,44 +13,24 @@ def test_count_survived_passengers():
     pclass_filter = 'Любой'
     answer = count_passengers(data, pclass_filter)
 
-    assert answer == {'under_30': 1,
-                      'above_60': 1,
-                      'all': 3
+    assert answer == {'under_30': 33,
+                      'above_60': 33,
                       }
 
 
-def test_calculate_survival_rate():
-    pclass_filter = 'Любой'
-    suvivours = count_passengers(data, pclass_filter)
-
-    survival_rate = count_survival_rate(suvivours)
-    assert survival_rate == {
-        'survival rate above 60': 33,
-        'survival rate under 30': 33
-    }
-
-
 def test_count_survived_first_class():
-    pclass_filter = 1
+    pclass_filter = '1'
     answer = count_passengers(data, pclass_filter)
 
     assert answer == {'under_30': 0,
-                      'above_60': 1,
-                      'all': 1
+                      'above_60': 100,
                       }
 
 
 def test_count_survived_third_class():
-    pclass_filter = 3
+    pclass_filter = '3'
     answer = count_passengers(data, pclass_filter)
 
-    assert answer == {'under_30': 1,
+    assert answer == {'under_30': 50,
                       'above_60': 0,
-                      'all': 2
                       }
-
-
-def test_count_survived_empty():
-    answer = count_passengers([])
-
-    assert list(answer.values()) == [0, 0, 0]
